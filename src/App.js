e7873d94af4de864602c7470a8d86dfaf77c9a4c
astek <jeremy.brunel@bashroom.com>
2024-01-06 18:23:01 +0100

    feat: front v1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
+import wheelImage from './taureau.jpeg';
 
 function App() {
+  const [spinResult, setSpinResult] = useState(null);
+  const [spinning, setSpinning] = useState(false);
+  const [animationIndex, setAnimationIndex] = useState(0);
+  const [visiblePrize, setVisiblePrize] = useState(null);
+  const prizes = ['Un café', 'Un dessert du chef', 'Une salade', 'Un poulpe frais'];
+  const SERVER_URL = 'http://127.0.0.1:3000/turn-the-wheel';
+  
+
+  useEffect(() => {
+    if (spinning) {
+      setAnimationIndex(0); // Réinitialise l'index d'animation à 0
+      setVisiblePrize(null); // Réinitialise le prix visible
+      const interval = setInterval(() => {
+        setAnimationIndex((prevIndex) => {
+          if (prevIndex < prizes.length) {
+            setVisiblePrize(prizes[prevIndex]); // Affiche l'élément actuel
+            return prevIndex + 1;
+          } else {
+            clearInterval(interval);
+            setSpinning(false); // Arrête le spin lorsque l'animation est terminée
+            return prevIndex;
+          }
+        });
+      }, 1000); // Ajustez la durée de l'intervalle selon vos besoins
+      return () => clearInterval(interval);
+    }
+  }, [spinning, prizes.length]);
+
+  const spinWheel = async () => {
+    setSpinning(true);
+    setSpinResult(null); // Réinitialise le résultat du spin
+    try {
+      const response = await axios.get(SERVER_URL);
+      const result = response.data;
+      setSpinResult(result);
+    } catch (error) {
+      console.error('Error spinning the wheel:', error);
+    }
+  };
+
+  useEffect(() => {
+    if (spinning && animationIndex < prizes.length) {
+      const timeout = setTimeout(() => {
+        setVisiblePrize(prizes[animationIndex]);
+      }, 1000); // Ajustez la durée de l'intervalle selon vos besoins
+      return () => clearTimeout(timeout);
+    }
+  }, [spinning, animationIndex, prizes.length]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Escapade Gourmande</h1>
+      <h3>Participez pour un petit cadeau lors de votre prochaine visite!</h3>
+      <img src={wheelImage} alt="Wheel Image" style={{ width: '100%', maxWidth: '400px', margin: '20px 0' }} />
+      
+      {spinning ? (
+        <div>
+          <p>Traitement en cours...</p>
+          {/* Animation pour afficher le prix actuel */}
+          {visiblePrize && (
+            <div key={animationIndex} className="PrizeAnimation">
+              {visiblePrize}
+            </div>
+          )}
+        </div>
+      ) : (
+        <div>
+          <button onClick={spinWheel}>Cliquez pour jouer</button>
+          {spinResult !== null && (
+            <p>
+            {spinResult === -1
+              ? `Désolée, vous n'êtes pas autorisé à participer plusieurs fois dans la même journée.`
+              : <span dangerouslySetInnerHTML={{ __html: `Vous avez remporté : <strong>${prizes[spinResult]}</strong>!` }} />}
+          </p>
+          
+          )}
+        </div>
+      )}
     </div>
   );
 }
