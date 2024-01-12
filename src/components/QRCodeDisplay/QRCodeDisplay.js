1bf912c6d43b234d7dc0f5f05aea8831a04b1b4d
astek <jeremy.brunel@bashroom.com>
2024-01-12 23:26:21 +0100

    fix: qr scanner perform a ping...

--- src/components/QRCodeDisplay/QRCodeDisplay.js
+++ src/components/QRCodeDisplay/QRCodeDisplay.js
@@ -4,7 +4,7 @@ import QRCode from 'qrcode.react';
 const QRCodeDisplay = ({ dateTime, prize, voucherId }) => (
     <figure>
       {/* <QRCode id='qr-code' value={`${dateTime.toUTCString()} - ${prize} - ${voucherId}`} /> */}
-      <QRCode id='qr-code' value={`https://escapade-gourmande-le-jeu-back.vercel.app/validate-voucher/${voucherId}`} />
+      <QRCode id='qr-code' value={`https://escapade-gourmande-le-jeu-back.vercel.app/validate-voucher/  \n       enter code : ${voucherId}`} />
       {/* <figcaption>
         QR Code for {prize} - {dateTime.toLocaleString()}
       </figcaption> */}
