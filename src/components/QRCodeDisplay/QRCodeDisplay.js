5fefa67dab446b002666c23c59127c666c50ead0
astek <jeremy.brunel@bashroom.com>
2024-01-13 01:09:01 +0100

    remove coquille

--- src/components/QRCodeDisplay/QRCodeDisplay.js
+++ src/components/QRCodeDisplay/QRCodeDisplay.js
@@ -4,7 +4,6 @@ import QRCode from 'qrcode.react';
 const QRCodeDisplay = ({ dateTime, prize, voucherId }) => (
     <figure>
       {/* <QRCode id='qr-code' value={`${dateTime.toUTCString()} - ${prize} - ${voucherId}`} /> */}
-      escapade-gourmande-coupon-validateur-front
       <QRCode id='qr-code' value={`https://escapade-gourmande-coupon-validateur-front.vercel.app/${voucherId}`} />
       {/* <figcaption>
         QR Code for {prize} - {dateTime.toLocaleString()}
